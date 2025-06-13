7061148d94699fa3ff204de2bb19e922c683b3c9
Michael Rodriguez <michaelexileydg@gmail.com>
2025-06-12 20:07:14 -0500

    updating menu

--- src/components/layout/App.tsx
+++ src/components/layout/App.tsx
@@ -4,49 +4,143 @@ import { FileHandler } from "../common/Uploader";
 import { IconDisplay } from "../common/IconDisplay";
 import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import bgImage from "../../assets/img/3D-BG.gif";
+import bgImage from "../../assets/img/90s Icon Pack.png";
 import { BaseCanvas } from "../canvas/BaseCanvas";
 import { canvasAssets } from "../canvas/canvasAssets";
 import { PerkCanvas } from "../canvas/PerkCanvas";
 export function App() {
   const [files, setFiles] = useState<{ name: string; data: string }[]>([]);
-  const [canvasURLs, setCanvasURLs] = useState<{ name: string; data: string; id: number }[]>([]);
+  const [canvasURLs, setCanvasURLs] = useState<
+    { name: string; data: string; id: number }[]
+  >([]);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [activeTab, setActiveTab] = useState("perk");
 
-  const handleTabChange = () => {
+  const handleTabChange = (tab?: string) => {
+    if (tab) setActiveTab(tab);
     setFiles([]);
     setCanvasURLs([]);
     setIsProcessing(false);
   };
 
-  const handleFileUpload: React.Dispatch<React.SetStateAction<{ name: string; data: string }[]>> = (newFiles) => {
+  const handleFileUpload: React.Dispatch<
+    React.SetStateAction<{ name: string; data: string }[]>
+  > = (newFiles) => {
     setFiles(newFiles);
   };
 
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
       <h1 className="text-5xl font-bold my-4 leading-tight">PERK CREATOR</h1>
-      <h2 className="italic text-gray-600">Work in Progress, Pardon the jank</h2>
+      <h2 className="italic text-gray-600">
+        Work in Progress, Pardon the jank
+      </h2>
 
       <div className="flex justify-center items-center gap-8 mb-8">
         <img
           src={bgImage}
           alt="3D image"
-          className="p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
+          className="p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] w-[512px]"
         />
       </div>
 
       <div className="flex justify-center">
-        <Tabs defaultValue="perk" className="w-[400px] items-center" onValueChange={handleTabChange}>
-          <TabsList>
-            <TabsTrigger value="perk">Perks</TabsTrigger>
-            <TabsTrigger value="item">Items</TabsTrigger>
-            <TabsTrigger value="addon">Addons</TabsTrigger>
-            <TabsTrigger value="offering">Offerings</TabsTrigger>
-          </TabsList>
+        <div className="w-[600px] items-center radio">
+          <label>
+            <input
+              type="radio"
+              name="tab"
+              value="perk"
+              onChange={() => handleTabChange("perk")}
+              defaultChecked
+            />
+            <span className="option">
+              Perks
+              <span className="click"></span>
+            </span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="tab"
+              value="item"
+              onChange={() => handleTabChange("item")}
+            />
+            <span className="option">
+              Items
+              <span className="click"></span>
+            </span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="tab"
+              value="addon"
+              onChange={() => handleTabChange("addon")}
+            />
+            <span className="option">
+              Addons
+              <span className="click"></span>
+            </span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="tab"
+              value="offering"
+              onChange={() => handleTabChange("offering")}
+            />
+            <span className="option">
+              Offerings
+              <span className="click"></span>
+            </span>
+          </label>
 
-          <TabsContent value="perk">
-            {/* Only pass files to PerkCanvas if not currently processing */}
+          {activeTab === "perk" && (
+            <PerkCanvas
+              files={isProcessing ? [] : files}
+              setCanvasURLs={setCanvasURLs}
+            />
+          )}
+
+          {activeTab === "item" && (
+            <BaseCanvas
+              files={isProcessing ? [] : files}
+              setCanvasURLs={setCanvasURLs}
+              jsonEndpoint="https://michaelexile.github.io/DBD-IconsJSON/dbdItems.json"
+              getBackgroundImage={(rarity) => ({
+                bg: canvasAssets.item.backgrounds[rarity],
+                grad: canvasAssets.item.gradients[rarity],
+              })}
+            />
+          )}
+
+          {activeTab === "addon" && (
+            <BaseCanvas
+              files={isProcessing ? [] : files}
+              setCanvasURLs={setCanvasURLs}
+              jsonEndpoint="https://michaelexile.github.io/DBD-IconsJSON/dbdItemAddons.json"
+              getBackgroundImage={(rarity) => ({
+                bg: canvasAssets.addon.backgrounds[rarity],
+                grad: canvasAssets.addon.gradients[rarity],
+              })}
+            />
+          )}
+
+          {activeTab === "offering" && (
+            <BaseCanvas
+              files={isProcessing ? [] : files}
+              setCanvasURLs={setCanvasURLs}
+              jsonEndpoint="https://michaelexile.github.io/DBD-IconsJSON/dbdOfferings.json"
+              getBackgroundImage={(rarity) => ({
+                bg: canvasAssets.offering.backgrounds[rarity],
+                grad: canvasAssets.offering.gradients[rarity],
+              })}
+            />
+          )}
+
+          {/* <TabsContent value="perk">
+
             <PerkCanvas files={isProcessing ? [] : files} setCanvasURLs={setCanvasURLs} />
           </TabsContent>
 
@@ -85,7 +179,8 @@ export function App() {
               })}
             />
           </TabsContent>
-        </Tabs>
+*/}
+        </div>
       </div>
 
       <div className="flex justify-center items-center">
